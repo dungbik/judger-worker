1147bc091eff9572b5c446b365a3fcb37130a4d3
dungbik <q1w2e30630@gmail.com>
2022-09-15 10:22:52 +0900

    Feat: Modify C Run Config

--- src/main/java/com/yoonleeverse/judgerworker/constant/JudgerConstant.java
+++ src/main/java/com/yoonleeverse/judgerworker/constant/JudgerConstant.java
@@ -9,7 +9,7 @@ public class JudgerConstant {
     public static final String SUBMISSION_PATH = "/submission";
 
     public static final Map<String, RunConfig> RUN_CONFIG_MAP = Map.ofEntries(
-            new SimpleEntry<>("C", new RunConfig("main.c", "main", 3000, 10000, 256, "/usr/bin/gcc10-gcc -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c11 %s -lm -o %s", "%s"))
+            new SimpleEntry<>("C", new RunConfig("main.c", "main", 3000, 10000, 256, "/usr/bin/gcc -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c11 %s -lm -o %s", "%s"))
     );
 
 
