e7737cfe9ebd01563ca38b829331f19f0a7ed197
dungbik <q1w2e30630@gmail.com>
2022-10-04 15:03:33 +0900

    Feat: Add Python2 in RUN_CONFIG_MAP

--- src/main/java/com/yoonleeverse/judgerworker/constant/JudgerConstant.java
+++ src/main/java/com/yoonleeverse/judgerworker/constant/JudgerConstant.java
@@ -12,7 +12,8 @@ public class JudgerConstant {
 
     public static final Map<ProgrammingLanguage, RunConfig> RUN_CONFIG_MAP = Map.ofEntries(
             new SimpleEntry<>(C, new RunConfig("main.c", "main", 3000, 10000, 256, "/usr/bin/gcc %s -o %s -O2 -Wall -lm -static -std=gnu11 -DONLINE_JUDGE", "%s")),
-            new SimpleEntry<>(CPP, new RunConfig("main.cpp", "main", 3000, 10000, 256, "/usr/bin/g++ %s -o %s -O2 -Wall -lm -static -std=gnu++11 -DONLINE_JUDGE", "%s"))
+            new SimpleEntry<>(CPP, new RunConfig("main.cpp", "main", 3000, 10000, 256, "/usr/bin/g++ %s -o %s -O2 -Wall -lm -static -std=gnu++11 -DONLINE_JUDGE", "%s")),
+            new SimpleEntry<>(PYTHON2, new RunConfig("main.py", "/usr/bin/python2 main.pyc", 3000, 10000, 256, "/usr/bin/python2 -m py_compile %s", "%s"))
     );
 
 
